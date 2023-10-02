9fd456c7567f68fc86ceb4675e6cb08527767fb6
Kabbier <balinagkabbier@gmail.com>
2023-10-02 23:39:19 +0530

    Create view.js
    View JS file following the MVC pattern.

---
+++ JS/view.js
@@ -0,0 +1,185 @@
+// View
+
+export default class View {
+  //export default || in app.js 'import {View}'
+
+  // properties of class view
+  $ = {};
+  $$ = {};
+
+  // elements that we are using
+  constructor() {
+    this.$.menu = this.#qs('[data-id="menu"]');
+    this.$.menuBtn = this.#qs('[data-id="menu-btn"]');
+    this.$.menuItems = this.#qs('[data-id="menu-items"]');
+    this.$.resetBtn = this.#qs('[data-id="reset-btn"]');
+    this.$.newRoundBtn = this.#qs('[data-id="new-round-btn"]');
+    this.$.modal = this.#qs('[data-id="modal"]');
+    this.$.modalText = this.#qs('[data-id="modal-text"]');
+    this.$.modalBtn = this.#qs('[data-id="modal-btn"]');
+    this.$.turn = this.#qs('[data-id="turn"]');
+    this.$.p1Wins = this.#qs('[data-id="p1-wins"]');
+    this.$.p2Wins = this.#qs('[data-id="p2-wins"]');
+    this.$.ties = this.#qs('[data-id="ties"]');
+    this.$.grid = this.#qs('[data-id="grid"]');
+
+    // special case for multiple (array) elements
+    this.$$.squares = this.#qsAll('[data-id="square"]');
+
+    //UI-only event listeners
+    this.$.menuBtn.addEventListener("click", (event) => {
+      this.#toggleMenu();
+    });
+  }
+
+  render(game, stats) {
+    const { playerWithStats, ties } = stats;
+    const {
+      moves,
+      currentPlayer,
+      status: { isComplete, winner },
+    } = game;
+
+    this.#closeAll();
+    this.#clearMoves();
+
+    this.#updateScoreboard(
+      playerWithStats[0].wins,
+      playerWithStats[1].wins,
+      ties
+    );
+
+    this.#initializeMoves(moves);
+
+    if (isComplete) {
+      this.#openModal(winner ? `${winner.name} wins!` : "Tie!");
+      return;
+    }
+
+    this.setTurnIndicator(currentPlayer);
+  }
+
+  /**
+   * Register all the event listeners
+   */
+  bindGameResetEvent(handler) {
+    this.$.resetBtn.addEventListener("click", handler);
+    this.$.modalBtn.addEventListener("click", handler);
+  }
+
+  bindNewRoundEvent(handler) {
+    this.$.newRoundBtn.addEventListener("click", handler);
+  }
+
+  bindPlayerMoveEvent(handler) {
+    this.#delegate(this.$.grid, '[data-id="square"]', "click", handler);
+  }
+
+  /**
+   * DOM helper methods
+   */
+  #updateScoreboard(p1Wins, p2Wins, ties) {
+    this.$.p1Wins.innerText = `${p1Wins} wins`;
+    this.$.p2Wins.innerText = `${p2Wins} wins`;
+    this.$.ties.innerText = `${ties} ties`;
+  }
+
+  #openModal(message) {
+    this.$.modal.classList.remove("hidden");
+    this.$.modalText.innerText = message;
+  }
+
+  #closeMenu() {
+    this.$.menuItems.classList.add("hidden");
+    this.$.menuBtn.classList.remove("border");
+
+    const icon = this.$.menuBtn.querySelector("i");
+
+    icon.classList.add("fa-chevron-down");
+    icon.classList.remove("fa-chevron-up");
+  }
+
+  #closeModal() {
+    this.$.modal.classList.add("hidden");
+  }
+
+  #closeAll() {
+    this.#closeModal();
+    this.#closeMenu();
+  }
+
+  #clearMoves() {
+    this.$$.squares.forEach((square) => {
+      square.replaceChildren();
+    });
+  }
+
+  #initializeMoves(moves) {
+    this.$$.squares.forEach((square) => {
+      const existingMove = moves.find((move) => move.squareId === +square.id);
+
+      if (existingMove) {
+        this.#handlePlayerMove(square, existingMove.player);
+      }
+    });
+  }
+
+  #toggleMenu() {
+    this.$.menuItems.classList.toggle("hidden");
+    this.$.menuBtn.classList.toggle("border");
+
+    const icon = this.$.menuBtn.querySelector("i");
+
+    icon.classList.toggle("fa-chevron-down");
+    icon.classList.toggle("fa-chevron-up");
+  }
+
+  #handlePlayerMove(squareEl, player) {
+    const icon = document.createElement("i");
+    icon.classList.add("fa-solid", player.iconClass, player.colorClass);
+    squareEl.replaceChildren(icon);
+  }
+
+  // player 1 || 2
+  setTurnIndicator(player, opponent) {
+    // assigning vars to the icon and text
+    const icon = document.createElement("i");
+    const label = document.createElement("p");
+
+    // method to change the icon and text of the turn indicator
+    icon.classList.add("fa-solid", player.colorClass, player.iconClass);
+
+    label.classList.add(player.colorClass);
+
+    label.innerText = `${player.name}, you're up!`;
+
+    this.$.turn.replaceChildren(icon, label);
+  }
+
+  // Query Selection Methods
+  #qs(selector) {
+    const el = document.querySelector(selector);
+
+    if (!el) throw new Error("Could not find elements");
+
+    return el;
+  }
+
+  #qsAll(selector) {
+    const elList = document.querySelectorAll(selector);
+
+    if (!elList) throw new Error("Could not find elements");
+
+    return elList;
+  }
+
+  //  more performance efficient than looping through each square with its own event listener;
+  // adds an event listener to the parent element and then matches which square was actually clicked
+  #delegate(el, selector, eventKey, handler) {
+    el.addEventListener(eventKey, (event) => {
+      if (event.target.matches(selector)) {
+        handler(event.target);
+      }
+    });
+  }
+}
